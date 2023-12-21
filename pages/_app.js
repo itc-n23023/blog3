a147fc614b69abc33d24fd10ec5cac4ca1711283
itc-n23023 <n23023@std.it-collge.ac.jp>
2023-12-21 07:41:21 +0000

    chapter5 終了
    画像表示とソーシャルリンクメニューを表示に成功しました。

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import Layout from 'components/layout'
 import 'styles/globals.css'
 
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Layout>
