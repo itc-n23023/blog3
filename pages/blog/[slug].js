5ba0cc9e8e31bc6e8f6e3e68736ae73158784f5d
itc-n23023 <n23023@std.it-collge.ac.jp>
2024-01-12 07:50:01 +0000

    senpaijogenn

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,14 +1,14 @@
 export async function getStaticPaths () {
   return {
     paths: [{ params: { slug: 'schedule' } }, { params: { slug: 'music' } }],
-    fallback: false
+    fallback: false,
   }
 }
 
-export async function getServerSideProps (context) {
-  console.log('params:', context.params)
+export async function getStaticProps(context) {
+  console.log('posts:', context.params)
   return {
-    props: { message: 'Dynamic Routes' },
+    props: { message: 'Dynamic Routes' }
     revalidate: 60,
   }
 }
