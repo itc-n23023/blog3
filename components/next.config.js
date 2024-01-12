db85d734f00d49cf450c308c80f818948881ca74
itc-n23023 <n23023@std.it-collge.ac.jp>
2024-01-12 03:33:52 +0000

    chapter8finish

---
+++ components/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    demains: ['images.microcms-assets.io']
+  }
+}
+
+module.exports = nextConfig
