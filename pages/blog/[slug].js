54a9e583233e2f5b01eb87f7ff4b2f40de5dd302
itc-n23023 <n23023@std.it-collge.ac.jp>
2024-01-13 00:40:20 +0000

    vercelエラー修正3

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -7,8 +7,11 @@ export async function getStaticPaths () {
 
 export async function getStaticProps (context) {
   console.log('posts:', context.params)
+
   return {
-    props: { message: 'Dynamic Routes' },
+    props: {
+      message: 'Dynamic Routes'
+    },
     revalidate: 60
   }
 }
