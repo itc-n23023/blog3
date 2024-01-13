746a9ba557ae4a2f98ec7f6166787d0d059af1cb
itc-n23023 <n23023@std.it-collge.ac.jp>
2024-01-13 01:19:07 +0000

    vercelエラー修正4

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,17 +1,15 @@
-export async function getStaticPaths() {
+export async function getStaticPaths () {
   return {
     paths: [{ params: { slug: 'schedule' } }, { params: { slug: 'music' } }],
     fallback: false
-  };
+  }
 }
 
-export async function getStaticProps(context) {
-  console.log('posts:', context.params);
+export async function getStaticProps (context) {
+  // export async function getServerSideProps(context) {
+  console.log('posts', context.params)
 
   return {
-    props: {
-      message: 'Dynamic Routes'
-    },
-    revalidate: 60
-  };
+    props: { message: 'Dynamic Routes' }
+  }
 }
