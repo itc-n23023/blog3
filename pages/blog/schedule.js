edb315e1d2c678c3f716010cc3634f701ceccd47
itc-n23023 <n23023@std.it-collge.ac.jp>
2024-01-16 05:17:07 +0000

    chapter9.1 finish

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -8,6 +8,8 @@ import {
   TwoColumnSidebar
 } from 'components/two-column'
 import Image from 'next/image'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
 import PostCategories from 'components/post-categories'
 import ConvertBody from 'components/convert-body'
 import { extractText } from 'lib/extract-text'
@@ -41,6 +43,8 @@ const Schedule = ({
             height={eyecatch.height}
             sizes='(min-width: 1152px) 1152px, 100vw'
             priority
+            placeholder='blur'
+            blurDataURL={eyecatch.blurDateURL}
           />
         </figure>
 
@@ -61,16 +65,21 @@ const Schedule = ({
 export default Schedule
 
 export async function getStaticProps () {
-  const slug = 'schedule'
+  const slug = 'micro'
 
   const post = await getPostBySlug(slug)
   const description = extractText(post.content)
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDateURL = base64
+
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch: eyecatch,
       categories: post.categories,
       description: description
     }
