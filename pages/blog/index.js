b218105ecb02646ce5bf1a7a38c70245db9be601
itc-n23023 <n23023@std.it-collge.ac.jp>
2024-01-31 07:57:09 +0000

    chapter6.5 finish

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -5,7 +5,7 @@ import Meta from 'components/meta'
 const Blog = () => {
   return (
     <Container>
-      <Meta pageTitle='ブログ' />
+      <Meta pageTitle='ブログ' pageDesc='ブログ記事一覧' />
       <Hero title='Blog' subtitle='Recent Posts' />
     </Container>
   )
